84cb05992013d6956f0a923ac84cba7e7a2556df
blackout0346 <149482684+blackout0346@users.noreply.github.com>
2025-10-06 22:23:09 +0800

    Add files via upload

--- ContainerFigure.h
+++ ContainerFigure.h
@@ -3,15 +3,22 @@
 #include<vector>
 #include<array>
 #include "Curve.h"
+#include "Cube.h"
+#include "Elipse.h"
 class ContainerFigure
 {
 private:
+	std::vector<std::shared_ptr<Curve>> container;
 
 public:
-	void LevelContainer()
-	{
-		
 
+	void LevelContainer(int index)
+	{
+		for (auto& fig : container)
+		{
+			fig->Draw();
+		}
+	
 	};
 };
 
--- Curve.h
+++ Curve.h
@@ -9,12 +9,14 @@ struct Vector {
 class Curve
 {
 public:
+	
+
 
 	virtual ~Curve() = default;
 	virtual Vector GetPoint(float t) =0;
 	virtual Vector GetDerivatives(float t)=0;
-
-	
+	virtual bool IsEqual(const Curve& curve) const = 0;
+	virtual void Draw() const = 0;
 
 };
 
