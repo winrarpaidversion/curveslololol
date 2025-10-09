0a34947650eed188396735645e4d0c2dc0ccf610
blackout0346 <149482684+blackout0346@users.noreply.github.com>
2025-10-09 23:38:06 +0800

    Add files via upload

--- ContainerFigure.h
+++ ContainerFigure.h
@@ -8,17 +8,53 @@
 class ContainerFigure
 {
 private:
-	std::vector<std::shared_ptr<Curve>> container;
-
+	std::vector<std::vector<std::shared_ptr<Curve>>> container;
+	int CurrentLevel = 0;
 public:
-
-	void LevelContainer(int index)
+	void initializeContainers()
 	{
-		for (auto& fig : container)
+		container.resize(4);
+		container[0].push_back(std::make_shared<Cube>(0.0f, 0.0f, 0.0f));
+		container[0].push_back(std::make_shared<Cube>(2.0f, 1.0f, 0.0f));
+		container[0].push_back(std::make_shared<Cube>(-2.0f, -1.0f, 1.0f));
+
+		container[1].push_back(std::make_shared<Elipse>(0.0f, 0.0f, 0.0f, 0.0f, 3.0f, 2.0f, RED));
+		container[1].push_back(std::make_shared<Elipse>(0.0f, 0.0f, 0.0f, 2.0f, 2.0f, 1.0f, RED));
+		//container[2].push_back(std::make_shared<Helix>(0, 0, 0));
+
+	}
+	void NextCurve()
+	{
+		if (CurrentLevel < container.size()-1)
 		{
-			fig->Draw();
+			CurrentLevel++;
 		}
+	}
+	void PrevCurve()
+	{
+		if (CurrentLevel > 0)
+		{
+			CurrentLevel--;
+		}
+	}
+	void DrawCurrentContainer()
+	{
+		if (CurrentLevel >= 0 && CurrentLevel < container.size())
+		{
+			for (auto& fig : container[CurrentLevel])
+			{
+				fig->Draw();
+			}
+		}
+	}
+	int GetCurrentContainer()
+	{
+		return CurrentLevel;
+	}
+	int GetTotalContainer()
+	{
+		return container.size();
+	}
 	
-	};
 };
 
