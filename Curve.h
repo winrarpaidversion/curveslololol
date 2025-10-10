e02c603031d48ed23ef6d7e94e83b8170f7d3be4
blackout0346 <149482684+blackout0346@users.noreply.github.com>
2025-10-10 02:20:07 +0000

    Add files via upload

--- Curve.h
+++ Curve.h
@@ -1,6 +1,7 @@
 #pragma once
 #include<cmath>
 #include<vector>
+#include<raylib.h>
 struct Vector {
 	float x;
 	float y;
@@ -9,14 +10,10 @@ struct Vector {
 class Curve
 {
 public:
-	
-
-
 	virtual ~Curve() = default;
-	virtual Vector GetPoint(float t) =0;
-	virtual Vector GetDerivatives(float t)=0;
+	virtual Vector GetPoint(float t) = 0;
+	virtual Vector GetDerivatives(float t) = 0;
 	virtual bool IsEqual(const Curve& curve) const = 0;
 	virtual void Draw() const = 0;
-
 };
 
